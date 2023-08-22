24248e1ccdfd0ec50f44e92462c80e157069d7d0
database-luis <lui.gustavo12@gmail.com>
2023-08-22 11:21:10 -0400

    criacao de tabelas

--- js1/script.js
+++ js1/script.js
@@ -1,17 +1,38 @@
 var tabela = document.createElement('table');
 var cabecalho = document.createElement('thead');
-var corpo = document. createElement('tbody');
+var corpo = document.createElement('tbody');
+var num = 10;
+var i;
+var j;
 
 tabela.appendChild(cabecalho);
 tabela.appendChild(corpo);
 
+tabela.classList.add('tabela-borda');
+
 document.getElementById('test').appendChild(tabela);
 
-var linhaCabecalho = document.createElement('tr');
+for (i = 1; i <= num; i++) {
+    var linhaCabecalho = document.createElement('tr');
 
-var celulacabecalho1 = document.createElement('th');
+    var celulaCabecalho1 = document.createElement('th');
 
-celulacabecalho1.textContent = 'ola';
+    celulaCabecalho1.setAttribute('colspan', '2');
+    celulaCabecalho1.textContent = 'produtos de ' + i;
 
-linhaCabecalho.appendChild(celulacabecalho1);
-cabecalho.appendChild(linhaCabecalho);
+    for(j = 1; j <= num; j++){
+        var linhaCorpo = document.createElement('tr');
+        var celulaCorpo1 = document.createElement('td');
+        var celulaCorpo2 = document.createElement('td');
+
+        celulaCorpo1.textContent = i + ' x ' + j; 
+        celulaCorpo2.textContent = i * j;
+        linhaCorpo.appendChild(celulaCorpo1);
+        linhaCorpo.appendChild(celulaCorpo2);
+        corpo.appendChild(linhaCorpo);
+    }
+
+    linhaCabecalho.appendChild(celulaCabecalho1);
+    cabecalho.appendChild(linhaCabecalho);
+
+}
